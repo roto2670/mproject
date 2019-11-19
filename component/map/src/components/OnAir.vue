544a9b6fdc2e6a5ceb9d550229383f7a80fd25c5
Jslee <jslee@narantech.com>
2019-11-19 13:22:45 +0900

    Add on-air icon

---
+++ component/map/src/components/OnAir.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="onAir" class="onair-container">
+        <div v-if="getOnAir" class="onair-image"></div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'onAir',
+    components: {
+    },
+    props: {
+        isOnAir: {
+            type: Boolean
+        }
+    },
+    data() {
+        return {
+        }
+    },
+    computed: {
+        getOnAir() {
+            return !this.isOnAir;
+        }
+    },
+    methods: {
+    },
+    created() {
+    }
+}
+</script>
+<style>
+.onair-container {
+    position:absolute;
+    left: 90%;
+    top: 5%;
+    z-index: 5;
+}
+.onair-image {
+    width: 70px;
+    height: 70px;
+    background-size: 100%;
+    background-image: url('../../public/static/location/imgs/on-air.svg');
+    background-position: center center;
+    background-repeat: no-repeat;
+}
+</style>
